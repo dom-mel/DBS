64cf88bb31fded03a7197ad60567a8f341d63161
Dominik Eckelmann <deckelmann@gmail.com>
2012-04-27 15:30:01 +0200

    more ignored words

---
+++ queries.sql
@@ -0,0 +1,14 @@
+
+-- Anfrage mit Aggregation:
+-- Berechnung der Gesamtzahl der Seiten aller Bücher
+-- im Bestand; für jedes einzelne Exemplar
+SELECT SUM(V.seiten)
+FROM (SELECT SUM(B.seitenanzahl) * B.exemplare as seiten
+FROM t_gr01_buecher B
+GROUP BY B.exemplare, B.seitenanzahl) V;
+
+-- Anfrage mit Referenzierung (relational):
+-- Zuordnung von Büchern zu ihren Autoren
+SELECT au.name, au.vorname, b.*
+FROM t_gr01_autorinnen au, TABLE(au.voe) v
+JOIN t_gr01_buecher b ON b.isbn = v.isbn;
